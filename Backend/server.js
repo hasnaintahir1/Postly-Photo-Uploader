06436d48e441e513133969a335feef8eb7b31d7a
hasnaintahir1 <hasnaintahir605@gmail.com>
2026-09-06 06:35:33 -0700

    update API URL and fix delete button functionality

--- Backend/server.js
+++ Backend/server.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 const app = require('./src/app');
 const connectDB = require('./src/db/db');
 
-const dns = require('dns');
-dns.setDefaultResultOrder('ipv4first');
-dns.setServers(['8.8.8.8', '8.8.4.4']);
-
 connectDB();
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.use((req, res, next) => {
+  if (!connectDB.isConnected) {
+    return res.status(503).json({
+      message: 'Service Unavailable'
+    });
+  }
+  next();
 });
--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -1,9 +1,24 @@
 const mongoose = require('mongoose');
 
-async function connectDB(){
-    await mongoose.connect(process.env.MONGODB_URI);
+// async function connectDB(){
+//     await mongoose.connect(process.env.MONGODB_URI);
 
-    console.log('MongoDB connected');
-};
+//     console.log('MongoDB connected');
+// };
+
+let isConnected = false;
+
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        isConnected = true;
+        console.log('MongoDB connected');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+}
 
 module.exports = connectDB;
